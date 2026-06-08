1c8ce1884d153394d2303d965f7a220960911cb7
albertniyo <a.niyonseng@alustudent.com>
2026-06-08 11:24:41 +0200

    add exercise3 alphabet game

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+    char ltr = 'a';
+    while (ltr <= 'z')
+    {   putchar(ltr);   ltr++;
+    }
+    putchar('\n');
+	return (0);
+}
