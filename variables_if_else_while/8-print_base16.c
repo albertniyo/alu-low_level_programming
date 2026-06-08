d6d7393620baa188df85ccef6071e4d07ac84292
albertniyo <a.niyonseng@alustudent.com>
2026-06-08 12:44:27 +0200

    add exercise 8 hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char nber = '0';
+	char ltr = 'a';
+
+	while (nber <= '9')
+	{
+		putchar(nber);
+		nber--;
+	}
+
+	while (ltr <= 'f')
+	{
+		putchar(ltr);
+		ltr++;
+	}
+	putchar('\n');
+
+	return (0);
+}
