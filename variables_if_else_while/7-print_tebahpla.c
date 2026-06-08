6af326ff84b629b357fd694796d9a4bbfcaa7f5b
albertniyo <a.niyonseng@alustudent.com>
2026-06-08 12:33:35 +0200

    add exercise 7 Smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char ltr = 'z';
+
+	while (ltr >= 'a')
+	{
+		putchar(ltr);
+		ltr--;
+	}
+	putchar('\n');
+
+	return (0);
+}
