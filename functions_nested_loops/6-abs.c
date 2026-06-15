ea95120f3d79755a527f30fc49dd57fbe1e47b1b
albertniyo <a.niyonseng@alustudent.com>
2026-06-15 15:58:32 +0200

    add exercise 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value
+ * @c: function parameter
+ *
+ * Return: absolute value of c
+ */
+int _abs(int c)
+{
+	if (c < 0)
+		return (-c);
+	else
+		return (c);
+}
