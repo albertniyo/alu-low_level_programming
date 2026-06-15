766972b75ceb7ac4c0521eb52b2613d6403d6199
albertniyo <a.niyonseng@alustudent.com>
2026-06-15 15:18:54 +0200

    add: exercises 1 alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet - printing lowercase letter
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char ltr = 'a';
+
+	while (ltr <= 'z')
+	{
+		_putchar(ltr);
+		ltr++;
+	}
+	_putchar('\n');
+}
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
