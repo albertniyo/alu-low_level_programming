a6d05862e59d95bda514d09732b5764129f1b3bf
albertniyo <a.niyonseng@alustudent.com>
2026-06-15 16:42:03 +0200

    add exercise 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds 2 integers and returns the sum
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a, b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
