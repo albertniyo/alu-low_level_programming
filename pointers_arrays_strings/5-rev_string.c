da612851045439b7a10dcb6db86639e9c43b3e91
albertniyo <a.niyonseng@alustudent.com>
2026-06-27 16:39:22 +0200

    add exercise rev_string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - funcs that reverse our string
+ * @s: pointer to our string to reverse printing
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int l = 0;
+	int i;
+    int temp;
+
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+
+	for (i = 0; i < l / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] =temp;
+	}
+}
