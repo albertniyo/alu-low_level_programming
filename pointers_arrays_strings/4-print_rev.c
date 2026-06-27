f5a2a6c0ec8014228ecf036c7449d14c233efd0b
albertniyo <a.niyonseng@alustudent.com>
2026-06-27 16:08:47 +0200

    fix: i initialized before the loop

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,14 @@
 void print_rev(char *s)
 {
 	int l = 0;
+	int i;
 
 	while (s[l] != '\0')
 	{
 		l++;
 	}
 
-	for (int i = l; i >= 0; i--)
+	for (i = l - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
