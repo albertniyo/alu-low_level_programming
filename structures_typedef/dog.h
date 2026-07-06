0fa5f2a6a49fec22c9c1e495c3b98323b760a11c
albertniyo <a.niyonseng@alustudent.com>
2026-07-06 08:06:55 +0200

    add exercise poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure representing a dog
+ * @name: pointer for the dog's name
+ * @age: float representing the dog's age
+ * @owner: pointer for the owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
